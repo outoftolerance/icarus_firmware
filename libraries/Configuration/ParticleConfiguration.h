7e6029d5a8390bfc66e6c25c5d28a0c8156709fb
James Howard <james@outoftolerance.com>
2020-06-19 14:19:48 -0700

    Latest updates to particle code, working towards enabling telemetry parsing

---
+++ libraries/Configuration/ParticleConfiguration.h
@@ -0,0 +1,7 @@
+#ifndef ParticleConfiguration_h
+#define ParticleConfiguration_h
+
+#define AIRPLANE_MODE_ALTITUDE 304.8
+#define LOCATION_REPORT_TTL 60
+
+#endif
--- particle/particle.ino
+++ particle/particle.ino
@@ -1,28 +1,44 @@
-#define AIRPLANE_MODE_ALTITUDE 304.8
-#define LOCATION_REPORT_TTL 60
+#include <ParticleConfiguration.h>
+
+#include <SimpleUtils.h>
+#include <SimpleLog.h>
+#include <SimpleHDLC.h>
+#include <SimpleMessageProtocol.h>
+
+Stream& logging_stream = Serial;                            /**< Logging output stream, this is of type Serial_ */
+Stream& icarus_stream = Serial1;                            /**< Icarus stream, this is of type HardwareSerial */
+
+SimpleHDLC icarus(icarus_stream, &handleMessageCallback);   /**< HDLC messaging object, linked to message callback */
+SimpleLog logger(logging_stream, &rtc, LOG_LEVELS::INFO);   /**< Log object */
 
 void setup() 
 {
-    Serial.begin(57600);
-    Serial1.begin(57600);
+    //Start debug serial port
+    logger.init();
+    logger.event(LOG_LEVELS::INFO, "Particle systems starting...");
+
+    //Start radio modem Serial port
+    logger.event(LOG_LEVELS::INFO, "Starting Icarus serial port...");
+    static_cast<HardwareSerial&>(icarus_stream).begin(57600);
+    logger.event(LOG_LEVELS::INFO, "Done!");
+
+    logger.event(LOG_LEVELS::INFO, "Finished initialisation, starting program!");
 }
 
 void loop() 
 {
-    int current_altitude = 0;
+    SimpleUtils::TelemetryStruct current_telemetry;                 /**< Current telemetry */
+
     bool cellular_enabled = true;
-    String serial_data;
+
+    current_telemetry.altitude = 0.0;
 
     while(1)
     {
-        //Check for new data on serial port
-        if(Serial1.available())
-        {
-            serial_data = Serial1.readStringUntil('\n');
-        }
+        icarus.receive();
 
         //Check current altitude
-        if(current_altitude <= AIRPLANE_MODE_ALTITUDE)
+        if(current_telemetry.altitude <= AIRPLANE_MODE_ALTITUDE)
         {
             if(!cellular_enabled)
             {
@@ -38,13 +54,39 @@ void loop()
                 cellular_enabled = false;
             }
         }
+    }
+}
 
-        //Check if connected
-        if(cellular_enabled && Cellular.ready() && Particle.connected() && serial_data.length() > 0)
-        {
-            //Publish location report
-            Particle.publish("message", serial_data, LOCATION_REPORT_TTL, PRIVATE);
-            serial_data = "";
-        }
+void handleMessageCallback(hdlcMessage message)
+{
+    logger.event(LOG_LEVELS::DEBUG, "Received a message!");
+
+    switch(message.command)
+    {
+        case MESSAGE_TYPES::MESSAGE_TYPE_HEARTBEAT:
+            logger.event(LOG_LEVELS::DEBUG, "Received message: Heartbeat.");
+            handleMessageHeartbeat(message);
+            break;
+        case MESSAGE_TYPES::MESSAGE_TYPE_REPORT_TELEMETRY:
+            logger.event(LOG_LEVELS::DEBUG, "Received message: Position Report.");
+            handleMessageTelemetryReport(message);
+            break;
+    }
+}
+
+void handleMessageHeartbeat(hdlcMessage& message)
+{
+    logger.event(LOG_LEVELS::DEBUG, "Ignoring heartbeat message.");
+}
+
+void handleMessageTelemetryReport(hdlcMessage& message)
+{
+    logger.event(LOG_LEVELS::INFO, "Received telemetry report message");
+
+    //Check if connected
+    if(cellular_enabled && Cellular.ready() && Particle.connected())
+    {
+        //Publish location report
+        Particle.publish("TELEMETRY_REPORT", "yo", LOCATION_REPORT_TTL, PRIVATE);
     }
 }
