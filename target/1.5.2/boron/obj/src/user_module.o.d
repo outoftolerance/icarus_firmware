e79c1f15cd86803c6b5a6d7a1b92328bacee2f93
James Howard <james@outoftolerance.com>
2020-06-29 10:30:58 -0700

    Remove target folder and outputs from Particle Workbench

--- target/1.5.2/boron/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-c:/Users/James/Desktop/test/Test/target/1.5.2/boron//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
